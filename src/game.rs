c610445a1922bee399cadecd4c6cafd94cf9a07d
François Mockers <francois.mockers@vleue.com>
2025-05-08 23:56:43 +0200

    bevy_enhanced_input

--- src/game.rs
+++ src/game.rs
@@ -2,21 +2,24 @@ use std::f32::consts::TAU;
 
 use avian2d::prelude::*;
 use bevy::prelude::*;
+use bevy_enhanced_input::prelude::*;
 use rand::Rng;
 
 use crate::{GameAssets, GameState, LoadedLevel, level::Level};
 
 pub fn game_plugin(app: &mut App) {
-    app.add_systems(OnEnter(GameState::Game), display_level)
-        .add_systems(
-            FixedUpdate,
-            // (control_player, move_player).run_if(in_state(GameState::Game)),
-            control_player.run_if(in_state(GameState::Game)),
-        )
+    app.add_input_context::<ShipController>()
+        .add_systems(OnEnter(GameState::Game), display_level)
+        // .add_systems(
+        //     FixedUpdate,
+        //     // (control_player, move_player).run_if(in_state(GameState::Game)),
+        //     control_player.run_if(in_state(GameState::Game)),
+        // )
         .add_systems(
             Update,
             (collision, tick_explosion).run_if(in_state(GameState::Game)),
         );
+    // .add_observer(rotate);
 }
 
 #[derive(Component)]
@@ -139,41 +142,52 @@ fn display_level(
 //     }
 //     Ok(())
 // }
-fn control_player(
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut player: Query<
-        (
-            &Transform,
-            &mut AngularVelocity,
-            &mut LinearVelocity,
-            &Children,
-        ),
-        With<Player>,
-    >,
-    mut visibility: Query<&mut Visibility>,
-) -> Result {
-    let Ok((transform, mut angular_velocity, mut linear_velocity, children)) = player.single_mut()
-    else {
-        // No player at the moment, skip control logic
-        return Ok(());
-    };
-    if keyboard_input.pressed(KeyCode::KeyA) {
-        angular_velocity.0 += 0.2;
-    }
-    if keyboard_input.pressed(KeyCode::KeyD) {
-        angular_velocity.0 -= 0.2;
-    }
-    if keyboard_input.pressed(KeyCode::KeyW) {
-        linear_velocity.0 += transform.local_y().xy() * 2.0;
-        linear_velocity.0 = linear_velocity.0.clamp_length_max(200.0);
-        *visibility.get_mut(children[0])? = Visibility::Visible;
-    } else {
-        visibility
-            .get_mut(children[0])?
-            .set_if_neq(Visibility::Hidden);
-    }
-    Ok(())
-}
+// fn control_player(
+//     keyboard_input: Res<ButtonInput<KeyCode>>,
+//     mut player: Query<
+//         (
+//             &Transform,
+//             &mut AngularVelocity,
+//             &mut LinearVelocity,
+//             &Children,
+//         ),
+//         With<Player>,
+//     >,
+//     mut visibility: Query<&mut Visibility>,
+// ) -> Result {
+//     let Ok((transform, mut angular_velocity, mut linear_velocity, children)) = player.single_mut()
+//     else {
+//         // No player at the moment, skip control logic
+//         return Ok(());
+//     };
+//     if keyboard_input.pressed(KeyCode::KeyA) {
+//         angular_velocity.0 += 0.2;
+//     }
+//     if keyboard_input.pressed(KeyCode::KeyD) {
+//         angular_velocity.0 -= 0.2;
+//     }
+//     if keyboard_input.pressed(KeyCode::KeyW) {
+//         linear_velocity.0 += transform.local_y().xy() * 2.0;
+//         linear_velocity.0 = linear_velocity.0.clamp_length_max(200.0);
+//         *visibility.get_mut(children[0])? = Visibility::Visible;
+//     } else {
+//         visibility
+//             .get_mut(children[0])?
+//             .set_if_neq(Visibility::Hidden);
+//     }
+//     Ok(())
+// }
+
+#[derive(InputContext)]
+struct ShipController;
+
+#[derive(Debug, InputAction)]
+#[input_action(output = f32)]
+struct Rotate;
+
+#[derive(Debug, InputAction)]
+#[input_action(output = bool)]
+struct Thrust;
 
 // fn move_player(mut player: Query<(&mut Transform, &PlayerVelocity)>) {
 //     for (mut player_transform, player_velocity) in player.iter_mut() {
@@ -272,18 +286,73 @@ fn tick_explosion(
 }
 
 fn spawn_player(commands: &mut Commands, game_assets: &GameAssets) {
-    commands.spawn((
-        Sprite::from_image(game_assets.player_ship.clone()),
-        RigidBody::Dynamic,
-        Collider::circle(40.0),
-        AngularDamping(5.0),
-        Player,
-        // PlayerVelocity(Vec2::ZERO),
-        StateScoped(GameState::Game),
-        children![(
-            Sprite::from_image(game_assets.jets.clone()),
-            Transform::from_xyz(0.0, -40.0, -1.0),
-            Visibility::Hidden,
-        )],
-    ));
+    let mut actions = Actions::<ShipController>::default();
+
+    actions.bind::<Rotate>().to(Bidirectional {
+        positive: KeyCode::KeyA,
+        negative: KeyCode::KeyD,
+    });
+    actions.bind::<Thrust>().to(KeyCode::KeyW);
+
+    commands
+        .spawn((
+            Sprite::from_image(game_assets.player_ship.clone()),
+            RigidBody::Dynamic,
+            Collider::circle(40.0),
+            AngularDamping(5.0),
+            Player,
+            // PlayerVelocity(Vec2::ZERO),
+            StateScoped(GameState::Game),
+            children![(
+                Sprite::from_image(game_assets.jets.clone()),
+                Transform::from_xyz(0.0, -40.0, -1.0),
+                Visibility::Hidden,
+            )],
+            actions,
+        ))
+        .observe(rotate)
+        .observe(thrust)
+        .observe(thrust_stop);
+}
+
+fn rotate(
+    trigger: Trigger<Fired<Rotate>>,
+    mut player: Query<&mut AngularVelocity, With<Player>>,
+    time: Res<Time>,
+) -> Result {
+    let fixed_rate = 0.2;
+    let delta = time.delta().as_secs_f32();
+    let rate = fixed_rate / (1.0 / (60.0 * delta));
+    let mut angular_velocity = player.get_mut(trigger.target())?;
+    angular_velocity.0 += trigger.value.signum() * rate;
+
+    Ok(())
+}
+
+fn thrust(
+    trigger: Trigger<Fired<Thrust>>,
+    mut player: Query<(&Transform, &mut LinearVelocity, &Children), With<Player>>,
+    mut visibility: Query<&mut Visibility>,
+) -> Result {
+    let (transform, mut linear_velocity, children) = player.get_mut(trigger.target())?;
+    linear_velocity.0 += transform.local_y().xy() * 2.0;
+    linear_velocity.0 = linear_velocity.0.clamp_length_max(200.0);
+    visibility
+        .get_mut(children[0])?
+        .set_if_neq(Visibility::Visible);
+    Ok(())
+}
+
+fn thrust_stop(
+    trigger: Trigger<Completed<Thrust>>,
+    player: Query<&Children, With<Player>>,
+    mut visibility: Query<&mut Visibility>,
+) -> Result {
+    let children = player.get(trigger.target())?;
+
+    visibility
+        .get_mut(children[0])?
+        .set_if_neq(Visibility::Hidden);
+
+    Ok(())
 }
