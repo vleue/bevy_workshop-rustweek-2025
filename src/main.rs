85334f87636bc23b2e923dd7b3401f9587fb814b
François Mockers <francois.mockers@vleue.com>
2025-05-12 16:12:21 +0200

    intro

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use bevy::prelude::*;
+
+mod splash;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Bevy Workshop".into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .init_state::<GameState>()
+        .enable_state_scoped_entities::<GameState>()
+        .add_plugins(splash::splash_plugin)
+        .run();
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, States, Default)]
+enum GameState {
+    #[default]
+    Splash,
+    StartMenu,
 }
---
+++ src/splash.rs
@@ -0,0 +1,55 @@
+use bevy::prelude::*;
+
+use crate::GameState;
+
+pub fn splash_plugin(app: &mut App) {
+    app.add_systems(OnEnter(GameState::Splash), display_title)
+        .add_systems(Update, switch_to_menu.run_if(in_state(GameState::Splash)));
+}
+
+fn display_title(mut commands: Commands) {
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            flex_direction: FlexDirection::Column,
+            ..default()
+        },
+        children![
+            (
+                Text::new("Bevy Workshop"),
+                TextFont {
+                    font_size: 130.0,
+                    ..default()
+                },
+            ),
+            (
+                Text::new("Rust Week 2025"),
+                TextFont {
+                    font_size: 100.0,
+                    ..default()
+                },
+            )
+        ],
+        StateScoped(GameState::Splash),
+    ));
+
+    commands.insert_resource(SplashScreenTimer(Timer::from_seconds(2.0, TimerMode::Once)));
+}
+
+#[derive(Resource)]
+struct SplashScreenTimer(Timer);
+
+fn switch_to_menu(
+    mut next: ResMut<NextState<GameState>>,
+    mut timer: ResMut<SplashScreenTimer>,
+    time: Res<Time>,
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        next.set(GameState::StartMenu);
+    }
+}
